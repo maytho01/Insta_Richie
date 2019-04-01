02b9bb18ce833b5fe8ed9bc820b8f2d0e0dc340b
Lachlan Morgante <lachlan.morgante@gmail.com>
2019-04-01 15:11:26 +1030

    1st commit for PersonalInfo.cs

---
+++ InstaRichie/Models/PersonalInfo.cs
@@ -0,0 +1,33 @@
+﻿using SQLite.Net.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartFinance.Models
+{
+    class PersonalInfo
+    {
+        [PrimaryKey, AutoIncrement]
+        public int PersonalID { get; set; }
+
+        [NotNull]
+        public string FirstName { get; set; }
+
+        [NotNull]
+        public double LastName { get; set; }
+
+        [NotNull]
+        public string DOB { get; set; }
+
+        [NotNull]
+        public string Gender { get; set; }
+
+        [NotNull]
+        public string EmailAddress { get; set; }
+
+        [NotNull]
+        public string MobilePhone { get; set; }
+    }
+}
