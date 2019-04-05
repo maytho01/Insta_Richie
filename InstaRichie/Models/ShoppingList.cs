c6ead6417b5ff353ff9448d44853f59c69a74e16
maytho01 <tmayer@internode.on.net>
2019-04-05 22:49:05 +1030

    Starting Again, so Not sure what this is doing

---
+++ InstaRichie/Models/ShoppingList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite.Net.Attributes;
+
+
+namespace StartFinance.Models
+{
+    class ShoppingList
+    {
+
+        [PrimaryKey, AutoIncrement]
+        public int ShoppingItemID { get; set; }
+
+        [NotNull]
+        public string ShopName { get; set; }
+
+        [NotNull]
+        public string NameOfItem { get; set; }
+
+        [NotNull]
+        public string ShoppingDate { get; set; }
+
+        [NotNull]
+        public double PriceQuoted { get; set; }
+
+    }
+}
